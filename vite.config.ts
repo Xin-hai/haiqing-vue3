6098d48d04b60b782e2b5a2ee6c93006239988fb
Xin-hai <aa1940358283@163.com>
2023-10-24 21:50:50 +0800

    测试完成

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   // base 关于路径
-  base: '/haiqing-vue3/dist/',
+  // base: '/haiqing-vue3/dist/',
   plugins: [vue()]
 })
